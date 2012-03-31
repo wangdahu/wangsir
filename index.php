704763c0d40de78a172f5156cdf4b773a0e07e7f
wangdahu <553784858@qq.com>
2012-03-31 17:47:54 +0800

    个性签名

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
 
-echo "<h1>欢迎光临本店...!</h1>";
+echo "<div style='text-align: center; min-height: 700px;'><h1>欢迎光临本店...!</h1></div>
+      <div style='float: right; padding-right: 30px;sudo '>无良杂种  2012-03-31</div>";
