7f9175e68702d3cbb3b0bf24f8b2ac6b0d7aff05
wangdahu <553784858@qq.com>
2012-03-31 19:08:07 +0800

    position...

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 
-echo "<div style='text-align: center; min-height: 700px;'><h1>欢迎光临本店...!</h1></div>
-      <div style='float: right; padding-right: 30px;sudo '>无良杂种  2012-03-31</div>";
+echo "<div style='text-align: center; min-height: 700px;'><h1>欢迎光临寒舍...!</h1></div>
+      <div style='position:fixed;bottom:10px; right:30px;'>无良杂种  2012-03-31</div>";
