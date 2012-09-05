0b293831d65b4b2a87aadcd2e5d70d7937d6260f
wangdahu <553784858@qq.com>
2012-09-05 12:40:59 +0800

    本地测试可以灌水

--- getlocation.php
+++ getlocation.php
@@ -1,6 +1,9 @@
 <?php
 
 function get_location($ip) {
+    if($ip = '127.0.0.1'){
+        return '本地测试';
+    }
     $services = array(
         array('url' => "http://ip138.com/ips.asp?action=2&ip=$ip", 'reg' => '/<li>本站主数据：([\x{4e00}-\x{9fa5}]+)/u'),
         array('url' => "http://ip.qq.com/cgi-bin/searchip?searchip1=$ip", 'reg' => '/IP所在地为：<span>([\x{4e00}-\x{9fa5}]+)/u')
--- sign.php
+++ sign.php
@@ -4,10 +4,13 @@ try {
     if($_POST){
         // 写入文件
         $filename = 'data/viewRecord.php';
+        if(!file_exists($filename)){
+            @touch($filename);
+        }
         if(!is_writable($filename)) {
             throw new Exception('Permission denied');
         }
-        $recordData = unserialize(file_get_contents($filename));
+        $recordData = unserialize(file_get_contents($filename)) ? unserialize(file_get_contents($filename)) : array();
         $data = array(
             'name' => $_POST['name'],
             'desc' => $_POST['desc'],
@@ -15,14 +18,15 @@ try {
             'ip_addr' => $_POST['ip_addr'],
             'location' => $_POST['location']
         );
-        $first = $recordData['0'];
-        if($_POST['ip_addr'] != $_SERVER['REMOTE_ADDR']){
-            throw new Exception("同志们阿!不要乱搞阿!");
-        } else  if($_POST['ip_addr'] == $first['ip_addr'] && time() - $first['time'] < 50){
-            throw new Exception("不要灌水哦");
+        if($recordData && $data['ip_addr'] != '127.0.0.1'){
+            $first = $recordData['0'];
+            if($_POST['ip_addr'] != $_SERVER['REMOTE_ADDR']){
+                throw new Exception("同志们阿!不要乱搞阿!");
+            } else  if($_POST['ip_addr'] == $first['ip_addr'] && time() - $first['time'] < 50){
+                throw new Exception("不要灌水哦");
+            }
         }
         array_unshift($recordData, $data);
-
         $recordStr = serialize($recordData);
         file_put_contents($filename, $recordStr);
         echo json_encode(array("status"=>1));
