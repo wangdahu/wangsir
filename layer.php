dd66f55f783a0673d40f7f75e1fe36a55e5fd5f6
Brook Yang <uedsky@gmail.com>
2013-12-16 15:16:28 +0800

    fix position

--- layer.php
+++ layer.php
@@ -1,8 +1,8 @@
 <link type="text/css" rel="stylesheet" href="./css/style.css">
 <div class="floor layer">
   <form action="sign.php" method="post" id="sign_form">
-    <div style="position: fixed; text-align: left; margin: 25% 40%; background-color: #DDD; line-height: 30px; min-width: 300px; " >
-      <div style="background-color: #A7C5E2; padding-left:10px;"> What's Your Name?<span class="js-close" style="cursor: pointer; float: right; margin-right:3px; padding:0 4px 0;">⨉</span></div>
+    <div style="position: fixed; background-color: #DDD; line-height: 30px; min-width: 300px; left: 50%; top: 37.2%; margin-left: -150px; margin-top: -100px;" >
+      <div style="background-color: #A7C5E2; padding-left:10px;"> What's Your Name?<span class="js-close" style="cursor: pointer; float: right; text-align: center; height: 30px; width: 30px; font-size:18px;">×</span></div>
       <div style="margin: 5px 10px;">
         <input type="search" name="name" required placeholder="您的大名" autofocus size="10" maxlength="10" /><span style="color: red;">*</span> 到此一游!<br />
         <textarea name="desc" rows="4" cols="30" placeholder="好空好空，您老人家给点好点子咯！拒绝灌水!!!"></textarea><br />
