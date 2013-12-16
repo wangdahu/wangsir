49272851988e67c91952a06a6b3ca894a5a78876
wangdahu <553784858@qq.com>
2013-12-16 19:16:01 +0800

    etst

--- weixin/index.php
+++ weixin/index.php
@@ -24,7 +24,7 @@ class wechatCallbackapiTest
     public function responseMsg()
     {
         //get post data, May be due to the different environments
-        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+        $postStr = isset($GLOBALS["HTTP_RAW_POST_DATA"]) ? $GLOBALS["HTTP_RAW_POST_DATA"] : ''
 
         //extract post data
         if (!empty($postStr)){
