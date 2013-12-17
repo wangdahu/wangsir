ec6e3947c22302cac6e5c40d586f00c2b2d0cfb3
wangdahu <553784858@qq.com>
2013-12-17 11:49:13 +0800

    weather

--- weixin/index.php
+++ weixin/index.php
@@ -80,12 +80,17 @@ class wechatCallbackapiTest
 	public function weather($city){
 		include("data/city_api.php");
 		$city_code = $city_api[$city];
+		if($city_code) {
+			return $city_code;
+		}else {
+			return 11;
+		}
 		$json = file_get_contents("http://m.weather.com.cn/data/{$city_code}.html");
 		$weather = json_decode($json);
 		if(!$weather->weatherinfo) {
-			$contentStr = "非常抱歉，没有找到[{$keyword}] 的天气情况";
+			$contentStr = "非常抱歉，没有找到[{$city}] 的天气情况";
 		}else {
-			$contentStr = "您查询的[{$keyword}]天气";
+			$contentStr = "您查询的[{$city}]天气";
 		}
 		return $contentStr;
 	}
